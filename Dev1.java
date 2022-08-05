c788f1304fb4b01111e9767ec93c939cae2357fa
BASKAR.K <110029191+Baskarkumar2313@users.noreply.github.com>
2022-08-05 18:03:27 +0530

    Add files via upload

---
+++ Dev1.java
@@ -0,0 +1,22 @@
+/*
+authour name:baskar.K
+progrm title:while loop
+date        :05/08/2022
+
+*/
+
+
+
+
+class Dev1
+{
+public static void main(String[]args)
+{
+int i=10,k=40;
+while(i<k)
+{
+System.out.println(i);
+i++;
+}
+}
+}
